9b5ab835f7b8213c70ecf8793baf46ba5de37657
daniel-ortiz-a <daniel.ortiz@alianzaelectrica.com>
2025-04-01 16:08:46 -0600

    fix:(prisma-schema)solution_prisma_schema

---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient, User } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaClient) {}
+
+  async findAll(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: number, data: Partial<User>): Promise<User> {
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async remove(id: number): Promise<User> {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+
+  async assignCard(id: number, cardNumber: string): Promise<User> {
+    return this.prisma.user.update({
+      where: { id },
+      data: {
+        cards: {
+          create: {
+            cardNumber,
+            isActive: true,
+          },
+        },
+      },
+    });
+  }
+}
